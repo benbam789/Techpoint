462d5ae7ec93a06bb02eb0fb02a4ff6468613162
benbam789 <benanu1zaku.gmail.com>
2021-07-09 04:22:33 -0400

    Second Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
-    return (
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
+  return (
     <> 
-        
+      <nav className="navbar">   
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            WebName <i className="fab fa-typo3"></i>
+          </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className = 'nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className = 'nav-item'>
+            <Link to='/market' className='nav-links' onClick={closeMobileMenu}>
+              Market
+            </Link>
+          </li>
+          <li className = 'nav-item'>
+            <Link to='/social' className='nav-links' onClick={closeMobileMenu}>
+              Social
+            </Link>
+          </li>
+          <li className = 'nav-item'>
+            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+      </nav>
     </>
     )
 }
 
-export default Navbar
+export default Navbar;
